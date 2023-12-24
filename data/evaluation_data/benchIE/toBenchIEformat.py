a1b95f12987227f045d3ea6169716ae4376d107e
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-12-24 21:55:22 +0700

    Update toBenchIEformat.py

--- data/evaluation_data/benchIE/toBenchIEformat.py
+++ data/evaluation_data/benchIE/toBenchIEformat.py
@@ -7,7 +7,7 @@ with open('sample300_en.txt', 'r') as f:
 
 with open('output_extractions.txt', 'r') as fin, open('compactIE_1.2_new.txt', 'w') as fout:
     for line in fin.readlines():
-        sentence, extraction, score = line.split('\t')
+        sentence, extraction = line.split('\t')
         sentId = sent2id[sentence]
         try:
             arg1 = extraction[extraction.index('<arg1>') + 6:extraction.index('</arg1>')]
